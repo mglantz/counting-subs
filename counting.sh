fcdfdce67ab393d1c6f016dddd0f2b540f53cfc5
Magnus Glantz <sudo@redhat.com>
2021-05-27 10:53:20 +0200

    Update counting.sh

--- counting.sh
+++ counting.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # For JBoss EAP, counts memory and CPU of pods controlled by the JBoss EAP operator
 
+LABEL=$1
+
 totalmem=0
 totalcpu=0
-for item in $(oc get pods --field-selector status.phase=Running --selector app.kubernetes.io/managed-by=eap-operator --all-namespaces|grep -v NAME|awk '{ print $2 }'); do
+for item in $(oc get pods --field-selector status.phase=Running --selector $LABEL --all-namespaces|grep -v NAME|awk '{ print $2 }'); do
 	podmem=$(oc adm top pods --all-namespaces|grep $item|awk '{ print $4 }')
 	podmemclean=$(oc adm top pods --all-namespaces|grep $item|awk '{ print $4 }'|sed 's/Mi//g')
 	podcpu=$(oc adm top pods --all-namespaces|grep $item|awk '{ print $3 }')
