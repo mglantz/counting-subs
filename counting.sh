b6f9feb32fe450d9b5e4f1829faf86beb0da0757
Magnus Glantz <sudo@redhat.com>
2021-05-27 11:48:06 +0200

    Cleanup

--- counting.sh
+++ counting.sh
@@ -6,11 +6,9 @@ LABEL=$1
 totalmem=0
 totalcpu=0
 for item in $(oc get pods --field-selector status.phase=Running --selector $LABEL --all-namespaces|grep -v NAME|awk '{ print $2 }'); do
-	podmem=$(oc adm top pods --all-namespaces|grep $item|awk '{ print $4 }')
 	podmemclean=$(oc adm top pods --all-namespaces|grep $item|awk '{ print $4 }'|sed 's/Mi//g')
-	podcpu=$(oc adm top pods --all-namespaces|grep $item|awk '{ print $3 }')
 	podcpuclean=$(oc adm top pods --all-namespaces|grep $item|awk '{ print $3 }'|sed 's/m//g')
-	echo "$item consumes MEM: $podmem and CPU: $podcpu"
+	echo "$item consumes MEM: ${podmemclean}Mi and CPU: ${podcpuclean}m"
 	totalmem=$(expr $totalmem + $podmemclean)
 	totalcpu=$(expr $totalcpu + $podcpuclean)
 done
